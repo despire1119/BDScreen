c3a893fda2d14516d8ab64a9532486d1f8dd1097
MrW <MrW@wanghaorandeMacBook-Pro.local>
2020-03-12 09:38:58 +0800

    rebuild

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-bdscreen app is running!');
+    expect(page.getTitleText()).toEqual('ddd app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-bdscreen'`, () => {
+  it(`should have as title 'ddd'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-bdscreen');
+    expect(app.title).toEqual('ddd');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-bdscreen app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ddd app is running!');
   });
 });
